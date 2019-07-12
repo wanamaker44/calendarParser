d3b6f9714baf6373437a66fb1747d54fb9876626
wanamaker44 <wanamaker44@gmail.com>
2019-07-11 20:45:34 -0400

    Game class completed. Initial parsing.

---
+++ game.py
@@ -0,0 +1,5 @@
+class game:
+  def __init__(self, subject, date, time):
+    self.subject = subject
+    self.date = date
+    self.time = time
--- parser.py
+++ parser.py
@@ -1,5 +1,9 @@
+import game
 
 filepath = "C:/Users/bill/Workspace/scheduleInput.txt"
+
+newGame = game.game('subjectasfdkjdsalfkj', 'date', 'time')
+allGames = []
 
 with open(filepath) as fp:
    line = fp.readline()
@@ -7,5 +11,12 @@ with open(filepath) as fp:
    while line:
        line = line.split('\t')
        print(line)
+       homeTeam = line[4] == 'Highrock'
+       otherTeam = (line[4], line[6])[homeTeam]
+       subj = line[2], (' vs ', ' at ')[homeTeam], otherTeam
+       allGames.append(game.game(subj, line[1], line[3]))
        line = fp.readline()
        cnt += 1
+
+for aGame in allGames:
+	print(aGame.subject, ',', aGame.date, ',', aGame.time, ',', aGame.date, ',', aGame.time, 'FALSE')
