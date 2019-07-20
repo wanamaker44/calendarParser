5c9a7e5fc5c6709d0e4b7cb42cb8700582f76419
wanamaker44 <wanamaker44@gmail.com>
2019-07-20 13:05:22 -0400

    Some updates and added flask server dummy file.

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 class game:
-  def __init__(self, subject, date, time):
+  def __init__(self, subject, date, time, homeTeam):
     self.subject = subject
     self.date = date
     self.time = time
+    self.homeTeam = homeTeam
--- parser.py
+++ parser.py
@@ -1,35 +1,41 @@
 import game
-import datetime
+from datetime import datetime
 import calendar
+import time
 
 filepath = "C:/Users/bill/Workspace/scheduleInput.txt"
 allGames = []
 
 with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
-       line = line.split('\t')
-       homeTeam = line[4] == 'Highrock'
-       otherTeam = (line[4], line[6])[homeTeam]
-       subj = line[2]+(' vs ', ' at ')[homeTeam]+otherTeam
-       subj = subj.strip()
-       allGames.append(game.game(subj, line[1], line[3]))
-       line = fp.readline()
-       cnt += 1
+	line = fp.readline()
+	cnt = 1
+	while line:
+		line = line.split('\t')
+		homeTeam = line[4] == 'Highrock'
+		otherTeam = (line[4], line[6])[homeTeam]
+		subj = line[2]+(' vs ', ' at ')[homeTeam]+otherTeam
+		subj = subj.strip()
 
-fileName = 'output/' + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.csv'
+		gameYear = datetime.now().year
+		gameMonthNum = list(calendar.month_abbr).index(line[1].split('-')[1])
+		gameDayNum = line[1].split('-')[0]
+		formattedGameDate = str(gameMonthNum) + '/' + str(gameDayNum) + '/' + str(gameYear)
+		gameTime = line[3]
 
-f = open(fileName, "a")
+		allGames.append(game.game(subj, formattedGameDate, gameTime, homeTeam))
+		line = fp.readline()
+		cnt += 1
 
-f.write('SUBJECT, START DATE, START TIME, END DATE, END TIME, ALL DAY EVENT, DESCRIPTION, LOCATION, PRIVATE\n')
+fileName = 'output/' + datetime.now().strftime("%Y%m%d%H%M%S") + '.csv'
 
-gameYear = datetime.datetime.now().year
+#f = open(fileName, "a")
+
+#f.write('SUBJECT, START DATE, START TIME, END DATE, END TIME, ALL DAY EVENT, DESCRIPTION, LOCATION, PRIVATE\n')
 
 for aGame in allGames:
-	gameMonthNum = list(calendar.month_abbr).index(aGame.date.split('-')[1])
-	gameDayNum = aGame.date.split('-')[0]
-	newRow = aGame.subject+','+ str(gameMonthNum) + '/' + str(gameDayNum) + '/' + str(gameYear) + ','+aGame.time+','+aGame.date+','+aGame.time+',FALSE'
-	f.write(newRow)
 
-f.close()
+	newRow = aGame.subject+','+ aGame.date + ',' + aGame.time + ','+ aGame.date +',' + aGame.time + ',FALSE'
+#	f.write(newRow)
+	print(aGame.time)
+
+#f.close()
